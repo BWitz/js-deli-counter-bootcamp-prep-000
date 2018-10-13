93f95b577dd451a0b7889877fd2cd7d940b55e4a
Brian Horowitz <bhorowitz2014@gmail.com>
2018-10-13 05:40:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,25 @@ function currentLine(line) {
     return `The line is currently empty.`;
   } else if (line.length > 0) {
     return `The line is currently: 1. ${line[0]}, 2. ${line[1]}, 3. ${line[2]}`;
+    /*
+    for .. the length of the array
+      print the current person in the line
+    */
   }
 }
+
+
+let i = 1;
+
+function takeANumber(currentLine) {
+ currentLine.push(i);
+ return `Welcome! You're ticket number # ${i++}`;
+}
+
+// takeANumber(katzDeliLine) => "Welcome! You're ticket number #1" [1]
+// takeANumber(katzDeliLine) => "Welcome! you're ticket number #2" [1,2]
+
+// nowServing(katzDeliLine) => "Currently Serving: #1" [2]
+// nowServing(katzDeliLine) => "Currently Serving: #2" []
+
+// takeANumber(katzDeliLine) => "Welcome! You're ticket number #3" [3]
